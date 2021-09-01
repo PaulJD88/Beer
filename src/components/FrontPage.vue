8a53f4b7d7a77d26369a14329f63809ca72dfe2c
PaulJD88 <77843480+PaulJD88@users.noreply.github.com>
2021-09-01 21:49:10 +0100

    Params and Queries

--- src/components/FrontPage.vue
+++ src/components/FrontPage.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    Beers ABV Above
+    <select v-model="abvAbove">
+      <option value="1">One</option>
+      <option value="8">Eight</option>
+    </select>
     <button @click="getBeers">Get Beers</button>
+        <div class="beers" v-for="beer in beers" :key="beer.id">
+          {{beer.image_url}}
+        </div>
   </div>
 </template>
 
@@ -11,14 +19,22 @@ export default {
   props: {
     msg: String,
   },
+  data() {
+    return {
+      abvAbove: 1,
+      beers: [],
+    };
+  },
   methods: {
     getBeers() {
-      fetch('https://api.punkapi.com/v2/beers', {
+      fetch(`https://api.punkapi.com/v2/beers?abv_gt=${this.abvAbove}`, {
         method: 'GET',
       })
         .then((response) => response.json())
         .then((json) => {
           console.log(json);
+          this.beers = json;
+          console.log(this.abvAbove);
         });
     },
   },
