aa282ec94d7175eda0a57e8108140c33d65f28b6
PaulJD88 <77843480+PaulJD88@users.noreply.github.com>
2021-09-10 16:33:37 +0100

    Still Figuring It Out

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="home">
-    <h1>This is the home page</h1>
-    <button @click="randomBeer">Random Beer</button>
-    <div class="rBeers" v-for="randomB in beersRandom" :key="randomB.id"></div>
+    <h1>Do you like Beer?</h1>
+
+    <input v-model="beersRandom" value="yes" type="radio" id="yes" />
+    <label for="no"> Yes I do!</label><br />
+
+    <input v-model="beersRandom" value="no" type="radio" id="no" />
+    <label for="no"> No I do not!</label><br />
+
+    <p v-if="beersRandom === 'yes'">
+      Yaaaaaaaaaaaay! {{ randomBeer.image_url }}
+    </p>
+    <p v-else-if="beersRandom === 'no'">Booo!</p>
+    <br />
+    <div class="get beer">
+      <button @click="randomBeer">Get Beers</button>
+      <h1>{{ randomBeer.id }}</h1>
+      <img :src="randomBeer.image_url" />
+    </div>
   </div>
 </template>
 
@@ -15,7 +30,7 @@ export default {
   data() {
     return {
       rBeer: 1,
-      beersRandom: [],
+      beersRandom: '',
     };
   },
   methods: {
@@ -26,7 +41,7 @@ export default {
         .then((response) => response.json())
         .then((json) => {
           console.log(json);
-          this.randomBeer = json;
+          this.beersRandom = json;
           console.log(this.rBeer);
         });
     },
